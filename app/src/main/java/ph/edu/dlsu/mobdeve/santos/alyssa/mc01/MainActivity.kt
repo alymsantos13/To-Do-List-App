cdf2f91a9a0ac3fcb819f552d9475a8c8e2ac116
Alyssa <alyssa_marie_santos@dlsu.edu.ph>
2022-05-01 18:50:24 +0800

    binding

--- app/src/main/java/ph/edu/dlsu/mobdeve/santos/alyssa/mc01/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/santos/alyssa/mc01/MainActivity.kt
@@ -2,10 +2,13 @@ package ph.edu.dlsu.mobdeve.santos.alyssa.mc01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.edu.dlsu.mobdeve.santos.alyssa.mc01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
