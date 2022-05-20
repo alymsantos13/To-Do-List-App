99379a885a93639b64079a6ffb2d71af31a848a4
Alyssa <alyssa_marie_santos@dlsu.edu.ph>
2022-05-20 20:06:20 +0800

    semi working.. set time should be implemented

--- app/src/main/java/ph/edu/dlsu/mobdeve/santos/alyssa/mc01/util/PrefUtil.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/santos/alyssa/mc01/util/PrefUtil.kt
@@ -6,9 +6,10 @@ import android.preference.PreferenceManager
 import ph.edu.dlsu.mobdeve.santos.alyssa.mc01.TimerActivity
 
 class PrefUtil {
-    private var appPreferences : SharedPreferences? = null
-    private val PREFS = "appPreferences"
+
     companion object{
+        private var appPreferences : SharedPreferences? = null
+        private val PREFS = "appPreferences"
 
         fun getTimerLength(context : Context) : Int {
             //Placeholder
@@ -18,13 +19,12 @@ class PrefUtil {
         private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "ph.edu.dlsu.mobdeve.santos.alyssa.timer.previous_timer_length"
 
         fun getPreviousTimerLengthSeconds(context : Context) : Long {
-            val PREFS = "appPreferences"
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val preferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
             return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
         }
 
         fun setPreviousTimerLengthSeconds(seconds: Long, context : Context){
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val editor = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE).edit()
             editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
             editor.apply()
         }
@@ -32,13 +32,13 @@ class PrefUtil {
         private const val TIMER_STATE_ID = "ph.edu.dlsu.mobdeve.santos.alyssa.timer.timer_state"
 
         fun getTimerState(context:Context) : TimerActivity.TimerState {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val preferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
             val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
             return TimerActivity.TimerState.values()[ordinal]
         }
 
         fun setTimerState(state: TimerActivity.TimerState, context : Context){
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val editor = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE).edit()
             val ordinal = state.ordinal
             editor.putInt(TIMER_STATE_ID, ordinal)
             editor.apply()
@@ -47,12 +47,12 @@ class PrefUtil {
         private const val SECONDS_REMAINING_ID = "ph.edu.dlsu.mobdeve.santos.alyssa.timer.previous_timer_length"
 
         fun getSecondsRemaining(context : Context) : Long {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val preferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
             return preferences.getLong(SECONDS_REMAINING_ID, 0)
         }
 
         fun setSecondsRemaining(seconds: Long, context : Context){
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val editor = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE).edit()
             editor.putLong(SECONDS_REMAINING_ID, seconds)
             editor.apply()
         }
@@ -60,12 +60,12 @@ class PrefUtil {
         private const val ALARM_SET_TIME_ID = "ph.edu.dlsu.mobdeve.santos.alyssa.timer.backgrounded_time"
 
         fun getAlarmSetTime(context: Context) : Long{
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val preferences = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
             return preferences.getLong(ALARM_SET_TIME_ID, 0)
         }
 
         fun setAlarmSetTime(time: Long, context: Context){
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val editor = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE).edit()
             editor.putLong(ALARM_SET_TIME_ID, time)
             editor.apply()
         }
